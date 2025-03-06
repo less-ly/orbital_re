d1edee74c7c03e939f6eea1c37f37f42a107e6bf
Mikhail Kiselev <m1k@tutamail.com>
2025-03-06 19:32:22 +0300

    It draws orbits now. Also adjusted the vertical position of the date somewhat.

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,5 +1,15 @@
 var rocky = require('rocky');
 
+function drawOrbits(ctx, cx, cy) {
+  var step = 1;
+  ctx.strokeStyle = 'black';
+  while (step < 4) {
+    ctx.beginPath();
+    ctx.arc(cx, cy, 20 * step, 0, 2 * Math.PI, false);
+    ctx.stroke();
+    step++;
+}};
+
 rocky.on('draw', function(event) {
   // Get the CanvasRenderingContext2D object
   var ctx = event.context;
@@ -21,18 +31,29 @@ rocky.on('draw', function(event) {
   // Center align the text
   ctx.textAlign = 'center';
 
+  // Set the font
+  ctx.font = '14px Gothic'
+
   // Get the current day of the month in 2-digit format
   var day = d.toLocaleTimeString(undefined, {day: '2-digit'});
   day = day.substring(0, 2);
 
   // Display the current date, in the middle of the screen
-  ctx.fillText(day, w / 2, h / 2, w);
-  
+  // adjust the heigt with the font's size
+  ctx.fillText(day, w / 2, h / 2 - 14 / 2, w);
+
+  // Determine the center point of the display
+  var cx = w / 2;
+  var cy = h / 2;
+
+  // Draw orbits
+  drawOrbits(ctx, cx, cy);
+ 
   // ctx.fillText(d.getDate().toString(), w / 2, h / 2, w);
   // getDate() doesn't put a preceding zero into 1-digit dates 
 });
 
-rocky.on('daychange', function(event) {
+rocky.on('secondchange', function(event) {
   // Request the screen to be redrawn on next pass
   rocky.requestDraw();
 });
